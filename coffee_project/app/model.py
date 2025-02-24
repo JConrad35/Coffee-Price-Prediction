6e82e3a1fdae70d486416cbf0aa95757b8ef1b80
Julian Conrad <julianhconrad01@gmail.com>
2025-02-23 20:34:42 -0700

    misc

--- coffee_project/app/model.py
+++ coffee_project/app/model.py
@@ -24,19 +24,18 @@ def make_prediction(model, scaler, input_data):
     """
     Make a prediction using the loaded model.
     """
-    input_data = np.array(input_data).reshape(-1, 1)  # Reshape input data to 2D array
-    input_data_scaled = scaler.transform(input_data).reshape(1, 10, 1)  # Scale and reshape to 3D array
-    prediction_scaled = model.predict(input_data_scaled)
+    input_data = np.array(input_data).reshape(1, 10, 1)  # Reshape input data to 3D array
+    prediction_scaled = model.predict(input_data)
     prediction = scaler.inverse_transform(prediction_scaled)  # Inverse transform to get unscaled prediction
     return prediction
 
+# Calculate RMSE
 def calculate_rmse(actual, predicted):
     """
     Calculate the Root Mean Squared Error (RMSE) between actual and predicted values.
     """
     rmse = np.sqrt(mean_squared_error(actual, predicted))
     return rmse
-
 
 # Example usage
 if __name__ == "__main__":
@@ -46,14 +45,13 @@ if __name__ == "__main__":
     model = load_model(model_path)
     scaler = load_scaler(scaler_path)
     
-    # Example input data (sequence of 10 time steps)
-    # from model_training import y_test_unscaled  # Import y_test_unscaled from model_training.py
-
-    input_data = y_test_unscaled[-11:-1]  # Use the last 11 values of y_test_unscaled, excluding the very last value
-    actual_value = [y_test_unscaled[-1]]  # Use the very last value of y_test_unscaled
-
+    input_data = y_test[-11:-1]  # Use the last 11 values of y_test, excluding the very last value
+    actual_value = [y_test[-1]]  # Use the very last value of y_test
+    actual_value = scaler.inverse_transform(np.array(actual_value).reshape(-1, 1))
     prediction = make_prediction(model, scaler, input_data)
+    
+    # Calculate and print RMSE
     rmse = calculate_rmse(actual_value, prediction)
-    print(f"RMSE: {rmse}")
+    print(f"Actual Value: {actual_value}")
     print(f"Prediction: {prediction}")
-    print(y_test_unscaled[-1])
+    print(f"RMSE: {rmse}")
